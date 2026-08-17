4621810114ebf3ff75aa48400cae261b3c3e1856
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-08-17 12:02:00 -0400

    Add files via upload

--- app/s/[shop]/layout.tsx
+++ app/s/[shop]/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./storefront.css";
+import "./theme.css";
 
 export default function StorefrontLayout({ children }: { children: ReactNode }) {
   return children;
